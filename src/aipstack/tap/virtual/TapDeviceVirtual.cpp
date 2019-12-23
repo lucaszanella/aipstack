3992f8e19a56f1098f49c8719ef9ffe831d62e0f
lz <me@lucaszanella.com>
2019-12-22 22:45:20 -0200

    backup

---
+++ src/aipstack/tap/virtual/TapDeviceVirtual.cpp
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2017 Ambroz Bizjak
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstring>
+#include <cstdio>
+#include <cerrno>
+#include <cstddef>
+#include <stdexcept>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <net/if_arp.h>
+#include <linux/if_tun.h>
+
+#include <aipstack/misc/Assert.h>
+#include <aipstack/misc/Function.h>
+#include <aipstack/infra/BufUtils.h>
+#include <aipstack/proto/EthernetProto.h>
+#include <aipstack/tap/virtual/TapDeviceVirtual.h>
+
+namespace AIpStack {
+
+TapDeviceVirtual::TapDeviceVirtual (
+    AIpStack::EventLoop &loop, std::string const &device_id, FrameReceivedHandler handler)
+:
+    m_handler(handler),
+    m_fd_watcher(loop, AIPSTACK_BIND_MEMBER(&TapDeviceVirtual::handleFdEvents, this)),
+    m_active(true)
+{
+    m_fd = AIpStack::FileDescriptorWrapper{::open("/dev/net/tun", O_RDWR)};
+    if (!m_fd) {
+        throw std::runtime_error("Failed to open /dev/net/tun.");
+    }
+    
+    m_fd.setNonblocking();
+    
+    std::string devname_real;
+
+    {
+        struct ifreq ifr;
+        std::memset(&ifr, 0, sizeof(ifr));
+        ifr.ifr_flags |= IFF_NO_PI|IFF_TAP;
+        std::snprintf(ifr.ifr_name, IFNAMSIZ, "%s", device_id.c_str());
+        
+        if (::ioctl(*m_fd, TUNSETIFF, reinterpret_cast<void *>(&ifr)) < 0) {
+            throw std::runtime_error("ioctl(TUNSETIFF) failed.");
+        }
+
+        devname_real = ifr.ifr_name;
+    }
+    
+    {
+        AIpStack::FileDescriptorWrapper sock{::socket(AF_INET, SOCK_DGRAM, 0)};
+        if (!sock) {
+            throw std::runtime_error("socket(AF_INET, SOCK_DGRAM) failed.");
+        }
+        
+        struct ifreq ifr;
+        std::memset(&ifr, 0, sizeof(ifr));
+        std::strcpy(ifr.ifr_name, devname_real.c_str());
+        
+        if (::ioctl(*sock, SIOCGIFMTU, reinterpret_cast<void *>(&ifr)) < 0) {
+            throw std::runtime_error("ioctl(SIOCGIFMTU) failed.");
+        }
+        
+        m_frame_mtu = std::size_t(ifr.ifr_mtu) + AIpStack::EthHeader::Size;
+    }
+    
+    m_read_buffer.resize(m_frame_mtu);
+    m_write_buffer.resize(m_frame_mtu);
+    
+    m_fd_watcher.initFd(*m_fd, AIpStack::EventLoopFdEvents::Read);
+}
+
+TapDeviceVirtual::~TapDeviceVirtual ()
+{}
+
+std::size_t TapDeviceVirtual::getMtu () const
+{
+    return m_frame_mtu;
+}
+
+AIpStack::IpErr TapDeviceVirtual::sendFrame (AIpStack::IpBufRef frame)
+{
+    if (!m_active) {
+        return AIpStack::IpErr::HardwareError;
+    }
+    
+    if (frame.tot_len < AIpStack::EthHeader::Size) {
+        return AIpStack::IpErr::HardwareError;
+    }
+    else if (frame.tot_len > m_frame_mtu) {
+        return AIpStack::IpErr::PacketTooLarge;
+    }
+    
+    char *buffer = m_write_buffer.data();
+    
+    std::size_t len = frame.tot_len;
+    frame = ipBufTakeBytes(frame, len, buffer);
+    
+    auto write_res = ::write(*m_fd, buffer, len);
+    if (write_res < 0) {
+        int error = errno;
+        if (AIpStack::FileDescriptorWrapper::errIsEAGAINorEWOULDBLOCK(error)) {
+            return AIpStack::IpErr::OutputBufferFull;
+        }
+        return AIpStack::IpErr::HardwareError;
+    }
+    if (std::size_t(write_res) != len) {
+        return AIpStack::IpErr::HardwareError;
+    }
+    
+    return AIpStack::IpErr::Success;
+}
+
+void TapDeviceVirtual::handleFdEvents (AIpStack::EventLoopFdEvents events)
+{
+    AIPSTACK_ASSERT(m_active);
+    
+    do {
+        if ((events & AIpStack::EventLoopFdEvents::Error) != AIpStack::Enum0) {
+            std::fprintf(stderr, "TapDeviceVirtual: Error event. Stopping.\n");
+            goto error;
+        }
+        if ((events & AIpStack::EventLoopFdEvents::Hup) != AIpStack::Enum0) {
+            std::fprintf(stderr, "TapDeviceVirtual: HUP event. Stopping.\n");
+            goto error;
+        }
+        
+        auto read_res = ::read(*m_fd, m_read_buffer.data(), m_frame_mtu);
+        if (read_res <= 0) {
+            bool is_error = false;
+            if (read_res < 0) {
+                int err = errno;
+                is_error = !AIpStack::FileDescriptorWrapper::errIsEAGAINorEWOULDBLOCK(err);
+            }
+            if (is_error) {
+                std::fprintf(stderr, "TapDeviceVirtual: read failed. Stopping.\n");
+                goto error;
+            }
+            return;
+        }
+        
+        AIPSTACK_ASSERT(std::size_t(read_res) <= m_frame_mtu);
+        
+        AIpStack::IpBufNode node{
+            m_read_buffer.data(),
+            std::size_t(read_res),
+            nullptr
+        };
+        
+        m_handler(AIpStack::IpBufRef{&node, 0, std::size_t(read_res)});
+    } while (false);
+    
+    return;
+    
+error:
+    m_fd_watcher.reset();
+    m_active = false;
+}
+
+}
---
+++ src/aipstack/tap/virtual/TapDeviceVirtual.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 Ambroz Bizjak
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef AIPSTACK_TAP_DEVICE_LINUX_H
+#define AIPSTACK_TAP_DEVICE_LINUX_H
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include <aipstack/misc/NonCopyable.h>
+#include <aipstack/misc/Function.h>
+#include <aipstack/misc/platform_specific/FileDescriptorWrapper.h>
+#include <aipstack/infra/Err.h>
+#include <aipstack/infra/Buf.h>
+#include <aipstack/event_loop/EventLoop.h>
+
+namespace AIpStack {
+
+class TapDeviceVirtual :
+    private AIpStack::NonCopyable<TapDeviceVirtual>
+{
+public:
+    using FrameReceivedHandler = Function<void(AIpStack::IpBufRef frame)>;
+
+    TapDeviceVirtual (AIpStack::EventLoop &loop, std::string const &device_id,
+                    FrameReceivedHandler handler);
+    
+    ~TapDeviceVirtual ();
+    
+    std::size_t getMtu () const;
+
+    AIpStack::IpErr sendFrame (AIpStack::IpBufRef frame);
+
+private:
+    void handleFdEvents (AIpStack::EventLoopFdEvents events);
+
+private:
+    FrameReceivedHandler m_handler;
+    AIpStack::FileDescriptorWrapper m_fd;
+    AIpStack::EventLoopFdWatcher m_fd_watcher;
+    std::size_t m_frame_mtu;
+    std::vector<char> m_read_buffer;
+    std::vector<char> m_write_buffer;
+    bool m_active;    
+};
+
+}
+
+#endif
